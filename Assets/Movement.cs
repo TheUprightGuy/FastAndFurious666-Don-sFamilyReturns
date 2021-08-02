1cc34e1dde1a21c99801cb3883d5444dd5a24d46
BigGriz <waydbarton@hotmail.co.nz>
2021-08-02 16:03:29 +1200

    CarRotation

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -8,7 +6,8 @@ public class Movement : MonoBehaviour
     public float speedForce;
     public float maxSpeed;
 
-    public float velocity;
+    public Transform car;
+    public float carRotation = 0.0f;
 
     private void Awake()
     {
@@ -18,27 +17,43 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        velocity = rb.velocity.magnitude;
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            if (rb.velocity.magnitude < maxSpeed)
-                rb.AddForce(transform.forward * speedForce);
-        }
-
         if (Input.GetKey(KeyCode.Space))
         {
             if (rb.velocity.magnitude > 0)
                 rb.velocity *= (1 - Time.deltaTime);
         }
+        else if (Input.GetKey(KeyCode.W) )
+        {
+            if (rb.velocity.magnitude < maxSpeed)
+                rb.AddForce(transform.forward * speedForce * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (rb.velocity.magnitude < maxSpeed)
+                rb.AddForce(-transform.forward * speedForce * Time.deltaTime * 0.75f);
+        }
+
+
+        carRotation = Mathf.Clamp(carRotation, -20.0f, 20.0f);
+        car.localRotation = Quaternion.Euler(0, carRotation, 0);
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(-Vector3.up * Time.deltaTime * 100.0f);
+            transform.Rotate(-Vector3.up * Time.deltaTime * 100.0f * Mathf.Clamp01(rb.velocity.magnitude / 10));
+
+            carRotation -= Mathf.Clamp01(rb.velocity.magnitude / 10.0f);
+            car.localRotation = Quaternion.Euler(0, carRotation, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.up * Time.deltaTime * 100.0f);
+            transform.Rotate(Vector3.up * Time.deltaTime * 100.0f * Mathf.Clamp01(rb.velocity.magnitude / 10));
+
+            carRotation += Mathf.Clamp01(rb.velocity.magnitude / 10.0f);
+            car.localRotation = Quaternion.Euler(0, carRotation, 0);
+        }
+        else
+        {
+            carRotation = Mathf.Lerp(carRotation, 0, Time.deltaTime * 3.0f);
         }
     }
 }
